e36a840f08bfbb0b084eea6795c7448daab85cad
Piam <piumchoke@gmail.com>
2024-12-03 16:21:48 +0700

    completed lab7_2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,33 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main() {
+    string n, m, d; 
+    int id; 
+    
+ 
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+    cout << "?????: ";
+    getline(cin, n); 
+    
+    cout << "Fahsai: Wow!!! " << n << " is a really cool name." << endl;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+    cout << n << ": ";
+    cin >> id; 
+    
+    cout << "Fahsai: I think you may be GEAR " << (id / 10000000) - 12 << ". I have a free movie ticket for you." << endl;
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+    cin.ignore(); 
+    cout << n << ": ";
+    getline(cin, m); 
+    
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+    cout << n << ": ";
+    getline(cin, d); 
+    
+    cout << "Fahsai: " << d << "....that is OK!!! I'm looking forward to watching " << m << " with you." << endl;
+    cout << "Luke Skywalker: May the Force be with you krub" << endl;
+    cout << "Fahsai: 555+ see you " << d << ". Bye Bye \\(^ ^)/" << endl;
+}
