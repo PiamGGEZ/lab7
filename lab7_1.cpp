e160693e6cdab61271dfbe9bb308cbc90e1db97e
Piam <piumchoke@gmail.com>
2024-12-03 16:41:27 +0700

    completed2 lab7_1

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -5,6 +5,6 @@ int main() {
     double a  =  20 , b = 30;
     double c = 4 , output;
     output = a - b/c;
-    cout << "output:" << output << "\n";
+    cout << "output: " << output << "\n";
     return 0;
 }
